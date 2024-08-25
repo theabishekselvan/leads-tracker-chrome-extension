b3206db6cae296250bb2fb85d373529f04666a47
abishek <115536896+theabishekselvan@users.noreply.github.com>
2024-08-26 00:07:00 +0530

    Added functionality to input field and button

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    inputEl.value = ""
+    renderLeads()
 })
 
-// Render the leads in the unordered list using ulEl.textContent
-for (let i = 0; i < myLeads.length; i++) { 
-    ulEl.textContent += myLeads[i] + " "
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        // Wrap the lead in an anchor tag (<a>) inside the <li>
+        // Can you make the link open in a new tab?
+        listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+    }
+    ulEl.innerHTML = listItems  
 }
-
-
-
